38984e2cb4de7ece4376f624d2b6a6bdc5460cce
k4ndar <k4ndar@yahoo.com>
2011-02-06 16:41:49 +0700

    Initiate offline documentation

--- apps/view/header.php
+++ apps/view/header.php
@@ -3,5 +3,21 @@
     >
 <html lang="en">
 <head>
-    <title><?php echo $page_title;?></title>
+<title><?php echo $page_title;?></title>
+<style type="text/css">
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    color: #454545;
+}
+
+a {
+    color: #1010CE; text-decoration: underline;
+}
+a:hover {
+    text-decoration: underline;
+}
+a:visited {
+    color: #1010CE; text-decoration: underline;
+}
+</style>
 </head>
--- apps/view/index.php
+++ apps/view/index.php
@@ -1,5 +1,11 @@
 <?php $this->view_header();?>
 <body>
-    <?php echo $body;?>
+    <h2>Panada has been Installed Successfully</h2>
+    <p><?php echo $body;?></p>
+    
+    To get more hint how to use Panada, see this offline <a href="documentation/id/index.html">documentation</a> page.<br />
+    To get latest update, follow our twitter <a target="_blank" href="http://twitter.com/panadaframework">@panadaframework</a>.<br />
+    To contribute the project, fork on github <a target="_blank" href="https://github.com/k4ndar/Panada">https://github.com/k4ndar/Panada</a>.<br />
+    For help and bug report, submit to <a target="_blank" href="https://github.com/k4ndar/Panada/issues">issues</a> page.<br />
 </body>
 </html>
