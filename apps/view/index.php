fe91979c7941e0ce4778fe4945e97b60a94e8fa7
k4ndar <k4ndar@yahoo.com>
2011-07-23 12:11:46 +0700

    add module link

--- apps/view/index.php
+++ apps/view/index.php
@@ -3,6 +3,7 @@
     <h2>Panada has been Installed Successfully</h2>
     <p><?php echo $body;?></p>
     
+    To see Hello World page from a module sample, <a href="index.php/mod_sample">click here.</a><br />
     To get more hint how to use Panada, see this offline <a href="documentation/id/index.html">documentation</a> page.<br />
     To get latest update, follow our twitter <a target="_blank" href="http://twitter.com/panadaframework">@panadaframework</a>.<br />
     To contribute the project, fork on github <a target="_blank" href="https://github.com/k4ndar/Panada">https://github.com/k4ndar/Panada</a>.<br />
