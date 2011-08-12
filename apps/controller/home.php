bfbf1c45029e566f81500944fece12b98534ecfa
k4ndar <k4ndar@yahoo.com>
2011-08-12 09:09:21 +0700

    Use absolute url path for css asset

--- apps/controller/home.php
+++ apps/controller/home.php
@@ -8,11 +8,11 @@ class Controller_home extends Panada {
     
     public function index(){
         $views['doc_type']      = $this->html->doctype('xhtml1-strict');
-        $views['css_file']      = $this->html->load_css('apps/asset/css/main.css', false);
+        $views['css_file']      = $this->html->load_css( $this->config->base_url() . 'apps/asset/css/main.css', false);
         $views['page_title']    = 'Hello World!';
         $views['head']          = 'Panada has been installed successfully!';
         $views['content']       = '<p>This is sample page. You find this file at:</p>';
-        $views['content']      .= '<code>'.pathinfo(APPLICATION, PATHINFO_BASENAME).'/view/home.php</code>';
+        $views['content']      .= '<code>'.pathinfo(APPLICATION, PATHINFO_BASENAME).'/view/index.php</code>';
         $views['content']      .= '<code>'.pathinfo(APPLICATION, PATHINFO_BASENAME).'/controller/home.php</code>';
         $views['footer']        = '<div id="foot">Powered by <a href="http://panadaframework.com/">Panada</a> version '.PANADA_VERSION.'';
         $views['footer']       .= '<span class="right">Page rendered in '.Library_time_execution::stop().' seconds</span></div>';
