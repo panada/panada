279e04d4353c38d74c979a8abba22cdf51f14fca
k4ndar <k4ndar@yahoo.com>
2011-10-13 15:51:46 +0700

    Index porting

--- apps/config/main.php
+++ apps/config/main.php
@@ -1,6 +1,6 @@
 <?php
 
 $main = array(
-    'gearPath' => '/home/kandar/Public/project/panada/v1proposal/panada/',
-    'modulePath' => '/home/kandar/Public/project/panada/v1proposal/panada/modules/'
+    'gearPath' => '../panada/',
+    'modulePath' => '../panada/modules/'
 );
