2a716306ac67d2ba866447d5b96a35d59c258d05
k4ndar <k4ndar@yahoo.com>
2011-11-07 20:23:18 +0700

    hmm

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,7 +7,6 @@ class Home extends Resources\Controller {
     public function index(){
         
         $data['title'] = 'Hello world!';
-        $data['mod'] = $this->model->sampleData->getData();
         
         $this->output('home', $data);
     }
