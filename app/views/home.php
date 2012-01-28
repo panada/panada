7b2c94d04a085009d53cd0f99482a924cab6765b
k4ndar <k4ndar@yahoo.com>
2012-01-28 11:57:27 +0700

    change Panada repo url to new one in sample page

--- app/views/home.php
+++ app/views/home.php
@@ -15,7 +15,7 @@
         <li>To check your system minimum requirements, please click this <a href="check.php">Checking Tools</a>.</li>
         <li>To see Hello World page from a module sample, <a href="<?php echo $this->location('modSample'); ?>">click here.</a></li>
         <li>To get latest update, follow our twitter <a target="_blank" href="http://twitter.com/panadaframework">@panadaframework</a>.</li>
-        <li>To contribute the project, fork on github <a target="_blank" href="https://github.com/panada/Panada">https://github.com/k4ndar/Panada</a>.</li>
+        <li>To contribute the project, fork on github <a target="_blank" href="https://github.com/panada/Panada">https://github.com/panada/Panada</a>.</li>
         <li>For help and bug report, submit to <a target="_blank" href="https://github.com/panada/Panada/issues">issues</a> page.</li>
     </ul>
     <p>
@@ -26,7 +26,7 @@
 
 <div id="foot">Powered by <a href="http://panadaframework.com/">Panada</a> version 1.0.0-nightly-build
 </div>
-<a href="http://github.com/k4ndar/Panada">
+<a href="http://github.com/panada/Panada">
 <img src="<?php echo $this->uri->baseUri; ?>assets/img/forkgithub.png" style="position: absolute; top: 0; right: 0; border: 0;" alt="Fork me on GitHub"></a>	
 </body>
 </html>
