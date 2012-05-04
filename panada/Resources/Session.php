bfa6a7266bf89b9ac477a8f502130c7b11c8922b
k4ndar <k4ndar@yahoo.com>
2012-05-04 16:46:43 +0700

    Add dynamic option setter for session resource

--- panada/Resources/Session.php
+++ panada/Resources/Session.php
@@ -18,8 +18,19 @@ class Session {
         
         $this->config = Config::session();
 	$this->config = $this->config[$connection];
-	$driverNamespace = 'Drivers\Session\\'.ucwords($this->config['driver']);
-        $this->driver = new $driverNamespace($this->config);
+	
+    }
+    
+    /**
+     * Overrider for session config option located at file app/config/session.php
+     *
+     * @param array $option The new option.
+     * @return void
+     * @since version 1.0
+     */
+    public function setOption( $option = array() ){
+	
+	$this->config = array_merge($this->config, $option);
     }
     
     /**
@@ -30,17 +41,44 @@ class Session {
      * @param array @arguments
      */
     public function __call($name, $arguments){
+	
+	$this->init();
         
         return call_user_func_array(array($this->driver, $name), $arguments);
     }
     
+    /**
+     * Magic getter for properties
+     *
+     * @param string
+     * @return mix
+     */
     public function __get($name){
         
         return $this->driver->$name;
     }
     
+    /**
+     * Magic setter for properties
+     *
+     * @param string
+     * @param mix
+     * @return mix
+     */
     public function __set($name, $value){
         
         $this->driver->$name = $value;
+    }
+    
+    /**
+     * Instantiate the driver class
+     *
+     * @return void
+     * @since version 1.0
+     */
+    private function init(){
+	
+	$driverNamespace = 'Drivers\Session\\'.ucwords($this->config['driver']);
+        $this->driver = new $driverNamespace($this->config);
     }
 }
