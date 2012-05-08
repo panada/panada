005da95d07c545442b1b698fdda49eaf0dd92c7f
de3 <naruto_vs_sousuke@yahoo.com>
2012-05-08 13:49:17 +0700

    Fix dynamic option setter for session resource
    function "init" is not invoked in the function __ call, but called in the function "construct" and "setOption"

--- panada/Resources/Session.php
+++ panada/Resources/Session.php
@@ -17,9 +17,20 @@ class Session {
     public function __construct($connection = 'default'){
         
         $this->config = Config::session();
-	$this->config = $this->config[$connection];
-	$driverNamespace = 'Drivers\Session\\'.ucwords($this->config['driver']);
-        $this->driver = new $driverNamespace($this->config);
+		$this->config = $this->config[$connection];
+		$this->init();
+    }
+    
+    /**
+     * Overrider for session config option located at file app/config/session.php
+     *
+     * @param array $option The new option.
+     * @return void
+     * @since version 1.0
+     */
+    public function setOption( $option = array() ){
+		$this->config = array_merge($this->config, $option);
+		$this->init();
     }
     
     /**
@@ -30,17 +41,42 @@ class Session {
      * @param array @arguments
      */
     public function __call($name, $arguments){
-        
+		
         return call_user_func_array(array($this->driver, $name), $arguments);
     }
     
+    /**
+     * Magic getter for properties
+     *
+     * @param string
+     * @return mix
+     */
     public function __get($name){
         
         return $this->driver->$name;
     }
     
+    /**
+     * Magic setter for properties
+     *
+     * @param string
+     * @param mix
+     * @return mix
+     */
     public function __set($name, $value){
         
         $this->driver->$name = $value;
+    }
+    
+    /**
+     * Instantiate the driver class
+     *
+     * @return void
+     * @since version 1.0
+     */
+    private function init(){
+	
+		$driverNamespace = 'Drivers\Session\\'.ucwords($this->config['driver']);
+        $this->driver = new $driverNamespace($this->config);
     }
 }
