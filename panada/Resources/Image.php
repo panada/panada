993e4929978b110819b7e018faed5db8aef65488
de3 <naruto_vs_sousuke@yahoo.com>
2012-06-01 16:33:17 +0700

    update option saveTo
    If option saveTo is set and option newFilename is not set, then
    filename automatically use original filename

--- panada/Resources/Image.php
+++ panada/Resources/Image.php
@@ -291,7 +291,7 @@ class Image {
         
         $file_extension = Upload::getFileExtension($this->fileName);
         $saveTo        = ( ! empty($this->saveTo) ) ? $this->saveTo : $this->folder;
-        $new_filename   = ( ! empty($this->newFileName) )? $saveTo . '/' . $this->newFileName . '.' . $file_extension : $this->filePath;
+        $new_filename   = ( ! empty($this->newFileName) )? $saveTo . '/' . $this->newFileName . '.' . $file_extension : $saveTo.'/'.$this->fileName;
         $new_filename   = ($this->stripSpaces) ? str_replace(' ', '_', $new_filename) : $new_filename;
         
         // move the new file
