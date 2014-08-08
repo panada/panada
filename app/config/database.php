a5748d56bd010affd43dc83d0d5bcce21e9f3926
Kandar <k4ndar@yahoo.com>
2014-08-08 22:12:00 +0700

    Change default MySQL driver to Mysqli

--- app/config/database.php
+++ app/config/database.php
@@ -3,7 +3,7 @@
 return array(
 
     'default' => array(
-        'driver' => 'mysql',
+        'driver' => 'mysqli',
         'host' => 'localhost',
         'port' => 3306,
         'user' => 'root',
