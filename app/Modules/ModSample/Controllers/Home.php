77837b2761ed06cec7cb9a1901b873032d5b21bc
Gemblue <ahmadoriza@gmail.com>
2015-04-18 23:26:49 +0700

    Modify modular sample, add load view option

--- app/Modules/ModSample/Controllers/Home.php
+++ app/Modules/ModSample/Controllers/Home.php
@@ -11,6 +11,17 @@ class Home extends Resources\Controller
 
     public function index()
     {
-        echo 'Hello world!';
+		$data['title'] = 'Hello world from "ModSample" module !<br/><br/>view path : /Panada/app/Modules/ModSample/Views/home.php';
+       
+		// Load view sample, from this module view file
+		$this->output('home', $data);
     }
+	
+	public function default_view_folder()
+	{
+		$data['title'] = 'Hello world from "ModSample" module !<br/><br/>view path : /Panada/app/views/home_mod_sample.php';
+		
+		// Load view sample, from default view file
+		$this->output('home_mod_sample', $data, false, true);
+	}
 }
