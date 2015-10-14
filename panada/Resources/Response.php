8500b935b102eb34faf84a42a4488acb76694230
Kandar <k4ndar@yahoo.com>
2015-10-14 22:47:43 +0700

    add response class

---
+++ panada/Resources/Response.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Resources;
+
+class Response
+{
+    public static $statusCode = 200;
+    public static $headers = [
+        'Content-Type' => 'text/html; charset=utf-8',
+    ];
+    public static $body;
+    
+    public function send()
+    {
+        self::sendHeader();
+        return self::$body;
+    }
+    
+    public function sendBody()
+    {
+        return self::$body;
+    }
+    
+    public static function sendHeader()
+    {
+        Tools::setStatusHeader(self::$statusCode);
+            
+        foreach(self::$headers as $name => $value) {
+            header($name.': '.$value, true);
+        }
+    }
+    
+    public function sendHeaders()
+    {
+        self::sendHeader();
+    }
+    
+    public function setBody($body)
+    {
+        self::$body = $body;
+    }
+    
+    public function setStatusCode($statusCode)
+    {
+        self::$statusCode = $statusCode;
+    }
+}
